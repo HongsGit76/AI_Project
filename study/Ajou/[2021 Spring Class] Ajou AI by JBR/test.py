032f9c9b60c1c867d45be1990c92854d2ee58697
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-03-25 00:46:35 +0900

    add data set

---
+++ study/Ajou/[2021 Spring Class] Ajou AI by JBR/test.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+x = np.load("C:/Users/sdat7/home/2021/인공지능/[2021 Spring Class] Ajou AI by JBR/test_gallery.npy")
+y = np.load("C:/Users/sdat7/home/2021/인공지능/[2021 Spring Class] Ajou AI by JBR/test_query.npy")
+z = np.load("C:/Users/sdat7/home/2021/인공지능/[2021 Spring Class] Ajou AI by JBR/train_label.npy")
+w = np.load("C:/Users/sdat7/home/2021/인공지능/[2021 Spring Class] Ajou AI by JBR/train.npy")
+
+print(x.shape)
+print(x.dtype)
---
+++ study/Ajou/[2021 Spring Class] Ajou ML by JBR/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+x = np.load("C:/Users/sdat7/home/2021/기계학습/[2021 Spring Class] Ajou ML by JBR/train_data.npy", allow_pickle=True)
+y = np.load("C:/Users/sdat7/home/2021/기계학습/[2021 Spring Class] Ajou ML by JBR/train_label.npy", allow_pickle=True)
+
+print()
