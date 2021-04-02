6481e1e66104f7c59abf3ecc5a09b7f270dacbcd
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-04-02 10:22:23 +0900

    machine learning assignment

--- study/Ajou/[2021 Spring Class] Ajou AI by JBR/test.py
+++ study/Ajou/[2021 Spring Class] Ajou AI by JBR/test.py
@@ -5,5 +5,10 @@ y = np.load("C:/Users/sdat7/home/2021/인공지능/[2021 Spring Class] Ajou AI b
 z = np.load("C:/Users/sdat7/home/2021/인공지능/[2021 Spring Class] Ajou AI by JBR/train_label.npy")
 w = np.load("C:/Users/sdat7/home/2021/인공지능/[2021 Spring Class] Ajou AI by JBR/train.npy")
 
-print(x.shape)
-print(x.dtype)
+# print(x.shape)
+# print(y.shape)
+# print(z.shape)
+print(w[0][0])
+
+# print(z)
+# print(w[0][0])
--- study/Ajou/[2021 Spring Class] Ajou ML by JBR/test.py
+++ study/Ajou/[2021 Spring Class] Ajou ML by JBR/test.py
@@ -1,6 +1,26 @@
 import numpy as np
+import torch
 
 x = np.load("C:/Users/sdat7/home/2021/기계학습/[2021 Spring Class] Ajou ML by JBR/train_data.npy", allow_pickle=True)
 y = np.load("C:/Users/sdat7/home/2021/기계학습/[2021 Spring Class] Ajou ML by JBR/train_label.npy", allow_pickle=True)
 
-print()
+# print(len(x))
+print(y[13])
+
+t = 0
+ft = 0
+with open("C:/Users/sdat7/home/git_commit/AI_Project/study/Ajou/[2021 Spring Class] Ajou ML by JBR/label.txt","r") as f:
+    while True:
+        line = f.readline()
+        if not line: break
+        answers = line.split(' ')
+        for i in answers:
+            a = i.split(",")
+            if len(a) == 1: break
+            if y[int(a[0])] == a[1]: t+=1
+            else:
+                print(f"{y[int(a[0])]}, {a[1]}")
+                ft+=1
+percent = t/(t+ft)
+
+print(percent)
