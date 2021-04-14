257eed48b72a3a2a21368489281b48d3128e34f1
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-04-14 22:48:51 +0900

    pytorch 튜토리얼

--- study/Ajou/[2021 Spring Class] Ajou AI by JBR/test.py
+++ study/Ajou/[2021 Spring Class] Ajou AI by JBR/test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 x = np.load("C:/Users/sdat7/home/2021/인공지능/[2021 Spring Class] Ajou AI by JBR/test_gallery.npy")
 y = np.load("C:/Users/sdat7/home/2021/인공지능/[2021 Spring Class] Ajou AI by JBR/test_query.npy")
@@ -8,7 +9,13 @@ w = np.load("C:/Users/sdat7/home/2021/인공지능/[2021 Spring Class] Ajou AI b
 # print(x.shape)
 # print(y.shape)
 # print(z.shape)
-print(w[0][0])
+# print(w.shape)
 
-# print(z)
 # print(w[0][0])
+plt.imshow(w[0][0])
+
+
+# print(w[0][0])
+
+# # print(z)
+# # print(w[0][0])
